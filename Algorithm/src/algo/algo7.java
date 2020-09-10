9f0db0d240f8fcba4f9c1548d6a11c2f752b4940
JunLyul_yang <sjn3649@gmail.com>
2020-09-10 18:00:44 +0900

    그래도 오늘 하나 풀었네

--- Algorithm/src/algo/algo7.java
+++ Algorithm/src/algo/algo7.java
@@ -1,34 +1,28 @@
 package algo;
 
-import java.util.Stack;
+import java.util.Arrays;
 
 public class algo7 {
 	public static void main(String[] args) {
-		//https://programmers.co.kr/learn/courses/30/lessons/42584?language=java
-		int [] prices = {1,2,3,2,3};
-		solution(prices);
+		//https://programmers.co.kr/learn/courses/30/lessons/1829
+		//어... 재귀적 함수인 것 같은데 살짝 난이도 미쳤는데...
+		//dfs라는 탐색법을 사용하는 것 같다. flood fill이라는 방식으로 풀 수 있다고 하는데... 난 그런 거 플루이드 필름 밖에 몰라
+		int [][] picture = {{1, 1, 1, 0}, {1, 2, 2, 0}, {1, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 3}, {0, 0, 0, 3}};
+		solution(6,4,picture);
 	}
 
-    public static int[] solution(int[] prices) {
-		int[] answer = new int[prices.length];
-		Stack<Integer> down = new Stack<Integer>();
-		int leng = 0;
-		
-		for(int i=0; i<prices.length;i++) {
-			leng = 0;
-			for(int j=i;j<prices.length;j++) {
-				leng++;
-				if(prices[i]>prices[j] || j==prices.length-1) {
-					down.push(leng-1);
-					break;
-				} 			
-			}
-		}
-		
-		for(int k=0;k<down.size();k++) {
-			answer[k] = down.get(k);
-		}
-		
-		return answer;
-	}
+    public static int[] solution(int m, int n, int[][] picture) {
+        int numberOfArea = 0;
+        int maxSizeOfOneArea = 0;
+
+        for(int [] pic: picture) {
+        	System.out.println(Arrays.toString(pic));
+        }
+        
+        
+        int[] answer = new int[2];
+        answer[0] = numberOfArea;
+        answer[1] = maxSizeOfOneArea;
+        return answer;
+    }
 }

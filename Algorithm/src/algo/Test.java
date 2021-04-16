274639f7bedff0b4f46c562b0d1438afda0dd0e8
JunLyul_yang <sjn3649@gmail.com>
2021-04-16 12:12:30 +0900

    70128

--- Algorithm/src/algo/Test.java
+++ Algorithm/src/algo/Test.java
@@ -1,38 +1,23 @@
 package algo;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Test {
 	public static void main(String[] args) {
-		int n = 45;
+		int [] a = {1,2,3,4};
+		int [] b = {-3,-1,0,2};
 		
-		System.out.println(Solution.solution(n));
+		System.out.println(Solution.solution(a,b));
 		
 	}
 }
 
 class Solution {
-	public static int solution(int n) {
-		int answer = 0;
-		int radix = 3; 
-		String convertedStr = "";
-		String reversedStr = "";
-		
-		while(n > 0) {
-			int quotient = n / radix;
-			int remainder = n % radix;
-			convertedStr = remainder + convertedStr;
-			n = quotient;
-		} 
-		
-		ArrayList<String> tempArr = new ArrayList<String>(Arrays.asList(convertedStr.split("")));
-		for(int i=0; i<tempArr.size(); i++) {
-			reversedStr = tempArr.get(i)+reversedStr;
-		}
-		
-		answer = Integer.valueOf(reversedStr, 3);
-		
-		return answer;
-	}
+    public static int solution(int[] a, int[] b) {
+        int answer = 0;
+        
+        for(int i=0; i<a.length; i++) {
+        	answer = answer + a[i]*b[i];
+        }
+        
+        return answer;
+    }
 }

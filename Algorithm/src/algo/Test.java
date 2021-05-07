469e4f800cd40b971a7bb1200a43ea61d3e1c34d
JunLyul_yang <sjn3649@gmail.com>
2021-05-07 17:56:30 +0900

    구명보트 문제 효율성 검사 통과 못해잇

--- Algorithm/src/algo/Test.java
+++ Algorithm/src/algo/Test.java
@@ -1,35 +1,55 @@
 package algo;
 
+import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
 
 public class Test {
 	public static void main(String[] args) {
-		int i = Solution.solution("cdcd");
+		int i[] = { 70, 50, 80, 50 };
 
-		System.out.println(i);
+		int r = Solution.solution(i, 100);
+		System.out.println(r);
+
 	}
 }
 
 class Solution {
-	public static int solution(String s) {
-		int answer = 0;
-		
-		Stack<Character> stack = new Stack<Character>();
-		
-		for(char c : s.toCharArray()) {
-			if(!stack.isEmpty() && stack.peek()==c) stack.pop();
-			else {
-				stack.push(c);
-			}
+	static LinkedList<Integer> q = new LinkedList<Integer>();
+	static int answer = 0;
+
+	public static int solution(int[] people, int limit) {
+
+		// 사전 처리
+		Arrays.sort(people);
+		for (int item : people) {
+			q.add(item);
 		}
-		
-		if(stack.size() == 0) {
-			answer = 1;
-		}
-		
+
+		Solution.help(limit);
 
 		return answer;
+	}
+
+	// 무거운 놈들부터 구하고, 구할 사람 남으면 재귀
+	public static void help(int limit) {
+
+		int man = q.removeLast();
+		int can = limit - man;
+		
+		if(!q.isEmpty() && q.peek() <= can) {
+			for (int i = q.size() - 1; i >= 0; i--) {
+				if (q.get(i) <= can) {
+					int guhal = q.get(i);
+					can = can - guhal;
+					q.removeLastOccurrence(guhal);
+					break;
+				}
+			}
+		}
+		answer++;
+		
+		if(!q.isEmpty()) {
+			help(limit);
+		}
 	}
 }

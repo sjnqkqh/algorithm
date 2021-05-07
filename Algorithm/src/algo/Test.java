2dc733d900ff13edabe39513b8ad5a2907bea69c
JunLyul_yang <sjn3649@gmail.com>
2021-05-07 14:18:13 +0900

    큐 이용해서 풀기 실패 다음엔 재귀로 풀어본다

--- Algorithm/src/algo/Test.java
+++ Algorithm/src/algo/Test.java
@@ -5,7 +5,9 @@ import java.util.Queue;
 
 public class Test {
 	public static void main(String[] args) {
-		Solution.solution("cdcd");
+		int i = Solution.solution("baabaa");
+	
+		System.out.println(i);
 	}
 }
 
@@ -15,11 +17,11 @@ class Solution {
         int answer = 0;
 
         // 원본 문자열
-        String [] originArr = s.split("");
-        Queue<String> ori = new LinkedList<String>();
-        Queue<String> temp = new LinkedList<String>();
+        char [] originArr = s.toCharArray();
+        Queue<Character> ori = new LinkedList<Character>();
+        Queue<Character> temp = new LinkedList<Character>();
 
-        for(String item : originArr) {
+        for(char item : originArr) {
         	ori.add(item);
         }
         
@@ -30,27 +32,65 @@ class Solution {
         
         // 큐에서 첫번째 인덱스와 두번째 인덱스 비교
         while(ori.size() > 1) {
-        	String a = ori.poll();
-        	String b = ori.peek();
+        	char a = ori.poll();
+        	char b = ori.peek();
         	
-        	if(!a.equals(b)) {
-        		temp.add(a);
+        	if(a != b){
+        		temp.offer(a);
         	} else {
         		ori.poll();
         	}
-
+        	
+        	// 문자열의 탐색이 모두 끝났고, 임시 문자열이 남아있을 경우 - 반복
         	if(ori.isEmpty() && !temp.isEmpty()) {
         		ori.addAll(temp);
         		temp.clear();
         	}
+        	
+        	// 문자가 하나만 남았을 경우 - 리턴 0
+        	if(ori.size()==1 && temp.isEmpty()) {
+        		return 0;
+        	}
+        	
+        	// 문자열이 하나 남았고, 임시 문자열이 남아있을 경우 - 반복할 수 없으므로 합치고 검증
+        	if(ori.size()==1 && !temp.isEmpty()) {
+        		temp.addAll(ori);
+        		ori.clear();
+        		ori.addAll(temp);
+        		temp.clear();
+        		
+        		if(ori.size() == 1) {
+        			return 0;
+        		}
+        		
+        		if(!deletableCheck(ori)) {
+        			return 0;
+        		}
+        		
+        	}
+
+        	
         }
         
-        if(ori.size() == 0) {
+        if(ori.isEmpty()) {
         	answer = 1;
         }
         
         return answer;
     }
     
-    
+    public static boolean deletableCheck(Queue<Character> q) {
+    	boolean result = false;
+    	
+    	while(q.size() > 1) {
+    		char a = q.poll();
+    		char b = q.peek();
+    		
+    		if(a == b) {
+    			return true;
+    		}
+    	}
+    	
+    	return result;
+    }
 }

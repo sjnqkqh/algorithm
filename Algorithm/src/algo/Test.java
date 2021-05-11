b911dc41040ade16362752b006749f3146662233
JunLyul Yang <sjn3649@gmail.com>
2021-05-11 21:23:57 +0900

    프로그래머스 60057 - 시간은 좀 걸렸어도 솔

--- Algorithm/src/algo/Test.java
+++ Algorithm/src/algo/Test.java
@@ -1,76 +1,105 @@
 package algo;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Test {
 	public static void main(String[] args) {
-		String record[] = { "Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo",
-				"Change uid4567 Ryan" };
+		String s = "xababcdcdababcdcd";
 
-		Solution.solution(record);
-
+		Solution.solution(s);
 	}
 }
 
 class Solution {
 
-	public static String[] solution(String[] record) {
-		String[] answer = {};
-		int ArrSize = record.length;
-		HashMap<String, String> nickMap = new HashMap<String, String>();
-
-		// 최종 별명 구하기
-		for (String item : record) {
-			String order[] = item.split(" ");
-			String orderStr = order[0];		// 명령어
-			String nameStr = order[1];		// 실제 아이디
-			String nickStr = "";			// 사용중인 닉네임
-			if (order.length == 3)	nickStr = order[2];
-
-			switch (orderStr) {
-			case "Enter":
-				nickMap.put(nameStr, nickStr);
-				break;
-				
-			case "Change":
-				nickMap.put(nameStr, nickStr);
-				ArrSize--;
-				break;
-			}
-		}
-
-		// 히스토리 남기기
-		answer = new String [ArrSize];
-		int index = 0;
-
-		for(int i=0; i<record.length; i++) {
-			String item = record[i];
-			String log = "";
-			
-			String order[] = item.split(" ");
-			String orderStr = order[0];
-			String nameStr = order[1];
-			
-			
-			switch (orderStr) {
-			case "Enter":
-				log = nickMap.get(nameStr)+"님이 들어왔습니다.";
-				answer[index] = log;
-				index++;
-				break;
-				
-			case "Leave":
-				log = nickMap.get(nameStr)+"님이 나갔습니다.";
-				answer[index] = log;
-				index++;
-				break;
-			}
-			
-		}
+	public static int solution(String s) {
+		int answer = s.length();
+		int max = s.length()/2;
 		
-		System.out.println(Arrays.toString(answer));
+		for(int i=1; i<=max ;i++) {
+			ArrayList<String> strArray = cut(s, i);
+			String zipedStr = duplicationCheck(strArray);
+			
+			if(zipedStr.length() < answer) {
+				answer = zipedStr.length();
+			}
+		}
 		
 		return answer;
+	}
+	
+	// 문자열 쪼개기
+	public static ArrayList<String> cut(String s, int range) {
+		ArrayList<String> result = new ArrayList<String>();
+		int strLength = s.length();
+		
+		for(int i=0; i<strLength; i++) {
+			int startNum = i*range;
+			int endNum	 = (startNum+range > strLength)? strLength : startNum+range;
+
+			if(startNum >= strLength || endNum > strLength) {
+				break;
+			}else {
+				String str = s.substring(startNum, endNum);
+				result.add(str);
+			}
+		}
+		return result;
+	}
+	
+	// 배열 순차탐색하면서 동일한 인자값 존재하는지 확인 후 문자열을 생성해 반환
+	public static String duplicationCheck(ArrayList<String> strArray){
+		Queue<String> strQueue = new LinkedList<String>();
+		int duplicationCount = 1;
+		String result = "";
+		
+		for(int i=0; i<strArray.size()-1; i++) {
+			String a = strArray.get(i);
+			String b = strArray.get(i+1);
+			
+			// A와 B가 같은 경우
+			// A-1. 해당 인덱스가 마지막 인덱스 (size()-2) 일 경우		=> 압축 후 큐에 삽입
+			// A-2. 해당 인덱스가 마지막 인덱스가 아닐 경우				=> 압축;
+			
+			// A와 B가 다른 경우
+			// B-1. 해당 인덱스가 마지막 인덱스 (size()-2) 일 경우		=> A와 B 모두 큐에 삽입
+			// B-2. 해당 인덱스가 마지막 인덱스가 아닐 경우				=> A만 큐에 삽입
+			
+			// A-1
+			if(a.equals(b) && i==strArray.size()-2) {
+				duplicationCount++;
+				strQueue.offer(String.valueOf(duplicationCount));
+				strQueue.offer(a);
+				duplicationCount = 1; // 압축횟수 초기화
+			}
+			
+			// A-2
+			if(a.equals(b) && i!=strArray.size()-2) {
+				duplicationCount++;
+			}
+			
+			// B-1
+			if(!a.equals(b) && i==strArray.size()-2) {
+				strQueue.offer(String.valueOf(duplicationCount));
+				strQueue.offer(a);
+				strQueue.offer(b);
+			}
+			
+			// B-2
+			if(!a.equals(b) && i!=strArray.size()-2) {
+				strQueue.offer(String.valueOf(duplicationCount));
+				strQueue.offer(a);
+				duplicationCount = 1; // 압축횟수 초기화
+			}
+		}
+
+		// 문자열로 변환
+		for(String item : strQueue) {
+			if(!item.equals("1")) result = result + item;
+		}
+		
+		return result;
 	}
 }
